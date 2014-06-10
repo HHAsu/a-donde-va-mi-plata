dc6489e0cb3de5f2d12499bbf1a93d9a314cc50d
Rafael Palau <rafaelpalau@gmail.com>
2014-06-10 00:55:49 -0400

    Change Welcome Message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -67,21 +67,97 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+<h1>
+<a name="user-content-a-d%C3%B3nde-va-mi-plata---plataformas-de-datos-abiertos-pgnpy" class="anchor" href="#a-d%C3%B3nde-va-mi-plata---plataformas-de-datos-abiertos-pgnpy"><span class="octicon octicon-link"></span></a>¿A dónde va mi plata? - PLATAFORMAS DE DATOS ABIERTOS PGNPY</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+<p>Es una herramienta de reporte y visualización de datos abiertos del Presupuesto General de la Nación (PGN). La misma cuenta con un conjunto de datos datos compuesto por los montos de ejecución presupuestaria desde el año 2003 al 2012 recabados del boost. Busca brindar a la ciudadanía un mecanismo sencillo y práctico de comparar los gastos oficiales del estado en distintas áreas claves para el desarrollo del país - salud, educación, gastos legislativos, gastos judiciales, obras, defensa y seguridad - y en distintos años.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+<p>Se busca evidenciar las tendencias de ejecución presupuestaria, tanto en tiempo como en funciones estatales. Contempla el desarrollo de una interfaz para poder generar reportes por parte de los usuarios finales</p>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
+<p>El producto final contempla facilidades para seleccionar distintos tipos de visualizaciones, así como también un mecanismo de sugerencia por parte de la ciudadanía de nuevos conjuntos de datos.</p>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+<h2>
+<a name="user-content-quienes" class="anchor" href="#quienes"><span class="octicon octicon-link"></span></a>Quienes?</h2>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+<ul class="task-list">
+<li>Rafael Palau - Líder de equipo de Desarrollo</li>
+<li>Michael Silvero - Líder de equipo de Periodismo</li>
+<li>Marcelo Elizeche - Desarrollador </li>
+<li>Rodrigo Valdez - Desarrollador</li>
+<li>Patricia Samudio - Diseñadora</li>
+<li>Santiago González - Periodismo Investigativo </li>
+<li>Gissela Peralta - Comunicadora</li>
+<li>Luisana Sanchez - Investigación</li>
+<li>PhD. Juan Pane - Consultor Experto</li>
+<li>Maricarmen Sequera y Jazmín Acuña - Coordinación</li>
+</ul><h2>
+<a name="user-content-para-quien" class="anchor" href="#para-quien"><span class="octicon octicon-link"></span></a>Para quien?</h2>
+
+<p>Explique a quién está dirigido, cuáles son sus usuarios finales y cuáles son sus características distintivas.
+Dado que el Presupuesto General es una herramienta de política gubernativa, mediante la cual se asignan recursos y determinan gastos para el cumplimiento de objetivos y políticas de estado para el desarrollo nacional en todas sus formas. La iniciativa se dirige a toda la ciudadanía en general.
+Esta herramienta es de puntual utilidad para investigadores, agencias de cooperación internacional, agentes tomadores de decisión en ambientes tanto públicos como privados, formadores de opinión, periodistas, comunicadores, analistas, estudiantes, organizaciones no gubernamentales, y ciudadanos y ciudadanas constructores y constructoras de una sociedad participativa e informada.</p>
+
+<h2>
+<a name="user-content-objetivos" class="anchor" href="#objetivos"><span class="octicon octicon-link"></span></a>Objetivos</h2>
+
+<p>¿A dónde va mi plata? tiene por objetivo facilitar a la ciudadanía el acceso y el análisis comparativo de los datos del presupuesto general de la Nación con una herramienta Web que visualice la información de forma simple organizada en tiempo y funciones estatales.</p>
+
+<h2>
+<a name="user-content-que-queremos" class="anchor" href="#que-queremos"><span class="octicon octicon-link"></span></a>Que queremos</h2>
+
+<p>Los/las usuarios/as de ¿A dónde va mi plata? podrán visualizar y comparar de qué manera se gastan los recursos del presupuesto en distintas áreas claves y en distintos años, pudiendo asi realizar distintos análisis y adquirir conciencia sobre el gasto público, a través de una interfaz web sencilla con reportes gráficos basados en técnicas de visualización de datos.</p>
+
+<h2>
+<a name="user-content-porque" class="anchor" href="#porque"><span class="octicon octicon-link"></span></a>Porque?</h2>
+
+<p>Existen datos del presupuesto general de la nación de manera pública, pero no son amigables, y son desconocidas por la ciudadanía en general. Esta nueva herramienta permitirá realizar consultas puntuales, así como también analizar, interpretar, examinar e investigar acerca de las tendencias de dichos gastos. Es la primera experiencia de visualización de datos públicos referentes a gastos del Estado paraguayo, con periodismo de dato que a través de un grupo multidiciplinario que es la incipiente comunidad Hacks Hackers Asunción.</p>
+
+<h2>
+<a name="user-content-que-mas-necesitamos" class="anchor" href="#que-mas-necesitamos"><span class="octicon octicon-link"></span></a>Que mas necesitamos?</h2>
+
+<p>Además de fondos, explique qué otras capacidades o apoyo necesita para desarrollar la propuesta que pone en consideración de HacksLabs de acuerdo al Producto Mínimo Viable descripto por Usted y al marco temporal establecido (hasta 2 meses de desarrollo para los grants de U$S 2,000 y hasta 4 meses para los grants medianos de U$S 10,000)</p>
+
+<p>Intercambio de experiencia con otros profesionales, cientificos de datos, expertos en periodismo de dato de la región para mejor orientación para ejecutar el proyecto.</p>
+
+<h2>
+<a name="user-content-con-que" class="anchor" href="#con-que"><span class="octicon octicon-link"></span></a>Con que?</h2>
+
+<p>Banco de datos desarrollado por el Ministerio de Hacienda de la República del Paraguay en cooperación con el Banco Mundial
+<a href="http://isdatabank.info/boost_paraguay/">http://isdatabank.info/boost_paraguay/</a></p>
+
+<h2>
+<a name="user-content-para-que" class="anchor" href="#para-que"><span class="octicon octicon-link"></span></a>Para que?</h2>
+
+<p>Mayor participación ciudadana en las políticas presupuestarias de la Nación. Empoderamiento a través de un adecuado y simple acceso a la información.
+Simplificación de herramientas para análisis e investigación de situación socio-económica.
+Investigaciones periodísticas en medios
+Participación ciudadana en la valoración de la gestión de las autoridades encargadas de la distribución del dinero.</p>
+
+<h2>
+<a name="user-content-como-idealmente" class="anchor" href="#como-idealmente"><span class="octicon octicon-link"></span></a>Como? (Idealmente)</h2>
+
+<ul class="task-list">
+<li>Financiamiento de Agencias de Cooperación Internacional en una primera etapa</li>
+<li>Alianzas estratégicas con entidades publicas</li>
+<li>Alianzas con medios (principalmente digitales)</li>
+</ul><p>El proyecto utilizará siempre software libre y las publicaciones estarán bajo la licencia Creative Commons. (CC0) (el dominio público de creative commons)</p>
+
+<h2>
+<a name="user-content-socios" class="anchor" href="#socios"><span class="octicon octicon-link"></span></a>Socios</h2>
+
+<ul class="task-list">
+<li>Ejempla: Disposición de periodistas, programadores y diseñadores. Difusión, espacio físico. <a href="http://ejempla.com">http://ejempla.com</a> Michael Silvero, <a href="mailto:mike@ejempla.com">mike@ejempla.com</a>, +595971582221</li>
+<li>TEDIC. <a href="http://www.tedic.org">www.tedic.org</a> ONG que desarrolla tecnologia cívica y defiende los derechos humanos en Internet. <a href="mailto:info@tedic.org">info@tedic.org</a> Asunción-Paraguay.</li>
+<li>Ministerio de Hacienda</li>
+<li>AAM - Santiago González (<a href="mailto:santiago@chena.com">santiago@chena.com</a> - +595971150600). Disposición de periodistas, editores, realizadores. Difusión en tv, radio y web con los espacios que sean necesarios para visibilizar el proyecto y analizar los datos que surjan desde el mismo. </li>
+<li>Canal 13 - Radio Cardinal AM y AAM tv. <a href="http://www.canal13.com.py">www.canal13.com.py</a> y <a href="http://www.cardinal.com.py">www.cardinal.com.py</a>
+</li>
+</ul><hr><p>Rafael Palau
+Estudiante de Ingeniería Informática.
+Actualmente elaborando Tesis de grado sobre Visualización y Apertura de Datos Públicos
+referentes al proceso de creación de PGN como herramienta de transparencia y toma de decisión informada.</p>
+
+<hr>
 </div>
 
 </body>
